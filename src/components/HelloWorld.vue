03b140959d73f335b5d71ba2e14f0225401cc0a2
Your Name <you@example.com>
2021-11-03 12:44:08 +0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,63 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+  <div id="table-demo" class="container">
+    <h3 class="p-3 text-center">Vue 3 - Display a list of items with v-for</h3>
+    <table>
+      <tr v-for="(row, x) in users" :key="x" :class="{ bold: x === 0 }">
+        <td
+          v-for="(col, y) in row"
+          :key="y"
+          :data-th="users[0][y].value"
+          @click="rowClicked(users, x, y)"
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+          {{ users[x][y].value }}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  name: "App",
+  setup() {
+    // make users variable reactive with the ref() function
+    const users = ref([
+      [
+        { value: "Fruit" },
+        { value: "Vegetable" },
+        { value: "Snacks" },
+        { value: "Drinks" },
+      ],
+      [
+        { value: "Apple" },
+        { value: "Celery" },
+        { value: "Chips" },
+        { value: "Beer" },
+      ],
+      [
+        { value: "Banana" },
+        { value: "Eggplant" },
+        { value: "Candy" },
+        { value: "Coke" },
+      ],
+      [
+        { value: "Oranges" },
+        { value: "Squash" },
+        { value: "Nuts" },
+        { value: "Tea" },
+      ],
+    ]);
+
+    return {
+      users,
+    };
+  },
+  methods: {
+    rowClicked(user, x, y) {
+      console.log(user[x][y].value + " is " + user[0][y].value);
+    },
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
